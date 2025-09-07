952d8289dfe4125d53c12a6a42f64b5b0b73eb0d
Maxmilian Sobr <msob973@aucklanduni.ac.nz>
2025-09-08 10:28:13 +1200

    Added functions to the speaker module to try and encapsulate all possible functions of the speaker and removed main test code

--- Pico_W_Project/main.c
+++ Pico_W_Project/main.c
@@ -5,48 +5,14 @@
 #include "common.h"
 #include "speaker/speaker.h"
 
-// UART defines
-// By default the stdout UART is `uart0`, so we will use the second one
-#define BAUD_RATE 9600
-
 int main()
 {
     stdio_init_all();
     
-    speakerUartInit();
+    speakerInitialise(15);
 
     cyw43_arch_init();
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-
-    speakerPlaySong(1, 1);
-    sleep_ms(10000);
-    speakerStopSong();
-    sleep_ms(5000);
-
-    speakerPlaySong(1, 2);
-    sleep_ms(10000);
-    speakerStopSong();
-    sleep_ms(5000);
-
-    speakerPlaySong(2, 1);
-    sleep_ms(10000);
-    speakerStopSong();
-    sleep_ms(5000);
-    
-    speakerPlaySong(2, 2);
-    sleep_ms(10000);
-    speakerStopSong();
-    sleep_ms(5000);
-
-    speakerPlaySong(1, 1);
-
-    for (uint8_t i=0; i<3; i++) {
-        speakerContinueSong();
-        sleep_ms(5000);
-        speakerPauseSong();
-        sleep_ms(2500); 
-    }
-    speakerStopSong();
 
     while (true) {
     }
--- Pico_W_Project/speaker/speaker.h
+++ Pico_W_Project/speaker/speaker.h
@@ -9,19 +9,22 @@
 #define SPEAKER_UART_TX_PIN 4
 #define SPEAKER_UART_RX_PIN 5
 
+/** The maximum volume allowed by the speaker */
+#define SPEAKER_MAX_VOLUME 30
+
 /* FOLDER_SONG_COUNTS used for random song selector.
 *  The value in index i = song count in folder number i+1
+*  These defines will change depending on what songs are in the speaker memory
 */
 #define USED_FOLDER_COUNT 2
 #define FOLDER_SONG_COUNTS {4, 2}
 #define TOTAL_SONGS 6
 
-/** Initialise the speaker UART using defines:
- *  SPEAKER_UART_ID,
- *  SPEAKER_UART_TX_PIN,
- *  SPEAKER_UART_RX_PIN
+/** Initialise the uart peripherals of the speaker and
+ *   all other initialisations to be able to use the speaker.
+ *  Volume = volume which the speaker should be set to <= 30
  */
-void speakerUartInit();
+void speakerInitialise(uint8_t volume);
 
 
 /** Send a command through the uart to the speaker
@@ -36,9 +39,17 @@ void speakerSendCommand(uint8_t command, uint8_t data_1, uint8_t data_2);
  */
 void speakerSendEmptyCommand(uint8_t cmd);
 
+/** Put speaker into sleep mode */
+void speakerSleepMode();
+
+/** Wake up the speaker from sleep mode */
+void speakerWakeUp();
+
 /** Play specified song in memory.
- *  - directory[0] = folder
- *  - directory[1] = song number in folder
+ *  - directory[0] = folder.
+ *      Folders start from 1 like in the speaker itself.
+ *  - directory[1] = song number in folder.
+ *      Song numbers start from 1 like in the speaker itself.
  */
 void speakerPlaySong(uint8_t folder, uint8_t song);
 
@@ -48,13 +59,36 @@ void speakerPauseSong();
 /** Continue the song that was playing before pausing */
 void speakerContinueSong();
 
+/** Play the next song in the folder */
+void speakerNextSong();
+
+/** Play the previous song in the folder */
+void speakerPreviousSong();
+
 /** Stop the currently playing song */
 void speakerStopSong();
 
+/** Change the volume to the specified value <= SPEAKER_MAX_VOLUME */
+void speakerSetVolume(uint8_t volume);
+
+/** Change the volume by the specified amount.
+ *  If it would exceed the lower or upper limits (0 <= Volume <= SPEAKER_MAX_VOLUME),
+ *   the volume is set to the limit instead
+ */
+void speakerChangeVolume(int8_t volume_change);
+
 /** Select and play a random song in the speaker.
- * The input is an array containing the number of songs in each folder.
- * - Value in index i = number of songs in folder number i+1.
+ *  This function depends on the defines 
+ *      USED_FOLDER_COUNT, FOLDER_SONG_COUNTS & TOTAL_SONGS
  */
 void speakerPlayRandomSong();
+
+/** Select and play a random song in a specified folder.
+ *      Folders start from 1 like in the speaker itself.
+ *  This function depends on the defines 
+ *      USED_FOLDER_COUNT & FOLDER_SONG_COUNTS
+ * 
+ */
+void speakerPlayRandomSongInFolder(uint8_t folder);
 
 #endif // SPEAKER_H

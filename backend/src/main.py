1c4b21e73b03f1e8e666211a0f48660c81d3c958
Ryan Tsai <90505632+requiemdev@users.noreply.github.com>
2025-09-12 14:44:57 +1200

    Implement speech rec module

--- backend/src/main.py
+++ backend/src/main.py
@@ -10,15 +10,24 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-from utils import FacialRec, OpenAiResponse, WakeWordListen
+from utils import FacialRec, OpenAiResponse, WakeWordListen, SpeechRec
+import serial
 
 def main():
     print("Backend running")
 
     try:
+        # ser = serial.Serial('COM3', 115200)  # Change COM3 to your Pico's port
         while True:
             if WakeWordListen.listenForWakeWord():
-                print("Wakeword detected, detecting face")
+                print("Wakeword detected, detecting speech")
+                speech = SpeechRec.recogniseSpeech()
+
+                if speech == "Error":
+                    # ser.write(0)
+                    print("Error")
+                    continue
+                print("Speech detected, detecting face")
                 emotion = FacialRec.detectEmotion()
                 print("Emotion detected: " + emotion)
 
@@ -26,5 +35,6 @@ def main():
                 res = OpenAiResponse.openAiResponse(emotion, "what a great day")
                 print(res)
     except KeyboardInterrupt:
+        # ser.close()
         print("Cancelled by user")
 main()

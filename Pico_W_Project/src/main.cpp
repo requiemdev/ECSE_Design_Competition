4f38d08b95df27b85084d577cee9609ee2d3a367
Maxmilian Sobr <msob973@aucklanduni.ac.nz>
2025-09-13 17:07:36 +1200

    Made OLED print "Detecting" text

--- Pico_W_Project/src/main.cpp
+++ Pico_W_Project/src/main.cpp
@@ -17,6 +17,8 @@ extern "C" {
 
 #include "microphone.h"
 #include "usb_microphone.h"
+#include "oled/ssd1306.h"
+#include "oled/font.h"
 
 #ifdef __cplusplus
 }
@@ -25,9 +27,7 @@ extern "C" {
 
 // Temporary includes for testing
 #include "command/command_list.h"
-#include "pin_output/gpio.h"
-#include "pin_output/pwm_functions.h"
-
+#include "hardware/i2c.h"
 
 // Global Variables
 volatile bool song_playing = true;
@@ -54,12 +54,33 @@ void mic_core() {
     }
 }
 
+// Test code
+void setup_gpios(void) {
+    i2c_init(i2c1, 400000);
+    gpio_set_function(2, GPIO_FUNC_I2C);
+    gpio_set_function(3, GPIO_FUNC_I2C);
+    gpio_pull_up(2);
+    gpio_pull_up(3);
+}
+
+
 int main()
 {
     MainEvent::initialiseMCU();
     current_state = State::SLEEP;
 
     multicore_launch_core1(mic_core);
+
+    // OLED Testing
+    char s[] = "Detecting";
+    setup_gpios();
+    ssd1306_t disp;
+    disp.external_vcc=false;
+    ssd1306_init(&disp, 128, 64, 0x3C, i2c1);
+    ssd1306_clear(&disp);
+    ssd1306_draw_string_with_font(&disp, 8, 24, 2, font_8x5, s);
+    // Make functions for "detecting", "listening", "playing"
+    ssd1306_show(&disp);
 
     while (true) {
         int8_t c = stdio_getchar_timeout_us(10);
